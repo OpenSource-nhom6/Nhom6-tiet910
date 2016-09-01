c4fd3425d16d6fb2ea800089957bd20fa836e32d
thanh0312 <thanh_0312199@yahoo.com>
2016-08-31 20:27:52 -0400

    Chỉnh khoảng cách

--- phanso.h
+++ phanso.h
@@ -3,30 +3,22 @@
       Duong Thi Thuy        Pham Quang Thanh
       Ngo Thi Duyen         Nguyen Ba Bao Tram
       Dang Trung Kien       Tran Thi Thu Huong
-    */
-    #ifndef _PhanSo_H
-        #define _PhanSo_H
-        class PhanSo
-        {
-            private: 
-                int Tu , Mau ;
-            private:
-               void ToiGian();
-            public:
-               PhanSo(int tu = 0, int mau = 1);
-               void Xuat() const;
-              PhanSo Chia(PhanSo b) const;
-               PhanSo Cong(PhanSo b) const ;
-               PhanSo Tru(PhanSo b) const ;
-               PhanSo Nhan(PhanSo b) const ;
-               bool SoSanh(PhanSo b) const ;
-               void Nhap ();
-       };
-       #endif
-    
-
-    
-    
-
-    
-
+   */
+   #ifndef _PhanSo_H
+   #define _PhanSo_H
+   class PhanSo
+   {
+      private: 
+         int Tu , Mau ;
+         void ToiGian();
+      public:
+         PhanSo(int tu = 0, int mau = 1);
+         void Xuat() const;
+         PhanSo Chia(PhanSo b) const;
+         PhanSo Cong(PhanSo b) const ;
+         PhanSo Tru(PhanSo b) const ;
+         PhanSo Nhan(PhanSo b) const ;
+         bool SoSanh(PhanSo b) const ;
+         void Nhap ();
+   };
+   #endif
